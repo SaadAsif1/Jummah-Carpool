66df14b7856260df33a790b0e8c006bfb44e0e1d
Saad Asif <saadasif2121@gmail.com>
2020-07-17 04:19:42 -0700

    changed about js

--- client/src/components/pages/About/About.js
+++ client/src/components/pages/About/About.js
@@ -1,49 +1,39 @@
-import React from "react";
-import HomeNavbar from "../../layouts/Home-Navbar/Navbar";
-import "./About.css";
+import React from 'react';
+import HomeNavbar from '../../layouts/Home-Navbar/Navbar';
+import './About.css';
 
 const About = () => {
   return (
-    <div className="home-container">
+    <div className='home-container'>
       <HomeNavbar />
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <div>aad</div>
-=======
-=======
->>>>>>> 54691d39658f32b34d2edfb7577656cce0fb2387
-      <div className="about-container">
-        <div className="about-p-container l-spacing">
-          <h2 className="about-p-title">About Sakeenat Ibad-ur-Rehman</h2>
-          <p className="about-p-body">
-            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local
-            community through in-person classes, and is outreaching muslims in
-            United States and globaly through its conference phone sessions.
+      <div className='about-container'>
+        <div className='about-p-container l-spacing'>
+          <h2 className='about-p-title'>About Sakeenat Ibad-ur-Rehman</h2>
+          <p className='about-p-body'>
+            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local community
+            through in-person classes, and is outreaching muslims in United States and
+            globaly through its conference phone sessions.
           </p>
         </div>
 
-        <div className="about-p-container l-spacing">
-          <h2 className="about-p-title">About Sakeenat Ibad-ur-Rehman</h2>
-          <p className="about-p-body">
-            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local
-            community through in-person classes, and is outreaching muslims in
-            United States and globaly through its conference phone sessions.
+        <div className='about-p-container l-spacing'>
+          <h2 className='about-p-title'>About Sakeenat Ibad-ur-Rehman</h2>
+          <p className='about-p-body'>
+            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local community
+            through in-person classes, and is outreaching muslims in United States and
+            globaly through its conference phone sessions.
           </p>
         </div>
 
-        <div className="about-p-container l-spacing">
-          <h2 className="about-p-title">About Sakeenat Ibad-ur-Rehman</h2>
-          <p className="about-p-body">
-            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local
-            community through in-person classes, and is outreaching muslims in
-            United States and globaly through its conference phone sessions.
+        <div className='about-p-container l-spacing'>
+          <h2 className='about-p-title'>About Sakeenat Ibad-ur-Rehman</h2>
+          <p className='about-p-body'>
+            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local community
+            through in-person classes, and is outreaching muslims in United States and
+            globaly through its conference phone sessions.
           </p>
         </div>
       </div>
-<<<<<<< HEAD
->>>>>>> 307bbb6b8f5d50414013a1baf38a4c3a6c834399
-=======
->>>>>>> 54691d39658f32b34d2edfb7577656cce0fb2387
     </div>
   );
 };
