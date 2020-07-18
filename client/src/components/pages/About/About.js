fc546f5dcb5b7765a43832c0366f255868a2fa79
umerp <pittalu5348@gmail.com>
2020-07-17 18:19:09 -0700

    Worked on about js page adding css and html

--- client/src/components/pages/About/About.js
+++ client/src/components/pages/About/About.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import HomeNavbar from '../../layouts/Home-Navbar/Navbar';
-
+import React from "react";
+import HomeNavbar from "../../layouts/Home-Navbar/Navbar";
+import "./About.css";
 const About = () => {
   return (
-    <div className='home-container'>
+    <div className="home-container">
       <HomeNavbar />
+
+      <div className="about-container">
+        <div className="about-section">
+          <h2>About Sakeenat Ibad-ur-Rehman</h2>
+          <p>
+            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local
+            community through in-person classes, and is outreaching muslims in
+            United States and globaly through its conference phone sessions.
+          </p>
+        </div>
+
+        <div className="about-section">
+          <h2>About Sakeenat Ibad-ur-Rehman</h2>
+          <p>
+            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local
+            community through in-person classes, and is outreaching muslims in
+            United States and globaly through its conference phone sessions.
+          </p>
+        </div>
+
+        <div className="about-section">
+          <h2>About Sakeenat Ibad-ur-Rehman</h2>
+          <p>
+            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local
+            community through in-person classes, and is outreaching muslims in
+            United States and globaly through its conference phone sessions.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
