b4a756e01acb08de9aad67fb6361accc464e5bbf
Abdalla Atalla <abdalla@abdallas-mbp.attlocal.net>
2020-07-22 16:01:16 -0700

    Cleaned up the how to page

--- client/src/components/pages/HowTo/HowTo.js
+++ client/src/components/pages/HowTo/HowTo.js
@@ -8,32 +8,9 @@ const About = () => {
       <HomeNavbar />
 
       <div className="about-container">
-        <div className="about-section">
-          <h2>About Sakeenat Ibad-ur-Rehman</h2>
-          <p>
-            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local
-            community through in-person classes, and is outreaching muslims in
-            United States and globaly through its conference phone sessions.
-          </p>
-        </div>
-
-        <div className="about-section">
-          <h2>About Sakeenat Ibad-ur-Rehman</h2>
-          <p>
-            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local
-            community through in-person classes, and is outreaching muslims in
-            United States and globaly through its conference phone sessions.
-          </p>
-        </div>
-
-        <div className="about-section">
-          <h2>About Sakeenat Ibad-ur-Rehman</h2>
-          <p>
-            Sakeenat Ibad-ur-Rehman has been serving thirteen years its local
-            community through in-person classes, and is outreaching muslims in
-            United States and globaly through its conference phone sessions.
-          </p>
-        </div>
+       <p>
+         Work in progress
+       </p>
       </div>
     </div>
   );
