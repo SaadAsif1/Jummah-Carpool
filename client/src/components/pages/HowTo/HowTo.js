5812f83dc8bd3b1e9179d4dcc4aa02ab8504cb78
Abdalla Atalla <abdalla@abdallas-mbp.attlocal.net>
2020-07-22 17:35:53 -0700

    Changed how topage to include youtube video

--- client/src/components/pages/HowTo/HowTo.js
+++ client/src/components/pages/HowTo/HowTo.js
@@ -6,11 +6,20 @@ const About = () => {
   return (
     <div className="home-container">
       <HomeNavbar />
-
       <div className="about-container">
-       <p>
-         Work in progress
-       </p>
+        <p>How to use Jummah Carpooling</p>
+        <div className="video-container">
+          <div>
+            <iframe
+              width="850"
+              height="543"
+              src="https://www.youtube.com/embed/za2I-fRi5D8"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
       </div>
     </div>
   );
